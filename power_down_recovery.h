9f5eee1ed87fc83bc9d917e8f8ff36dbd4191b32
RomanAlexandroff <r.aleksandroff@gmail.com>
2023-10-20 21:32:28 +0200

    add restart detection

--- power_down_recovery.h
+++ power_down_recovery.h
@@ -16,14 +16,41 @@
 
 void  ft_power_down_recovery(void)
 {
-    RTC_DATA_ATTR static long  power_loss_detector;
+    RTC_DATA_ATTR static long power_loss_detector;
+    esp_reset_reason_t        reason;
 
     if (power_loss_detector != 1110111)
     {
-        bot.sendMessage(CHAT_ID, "Hello! I am Soft Location Tracker. I am ON and ready for work!", "");
         g_last_wifi = 0;
         g_offline_wakeups = 0;
+        g_reboot = false;
         ft_delete_spiffs_file("/offline_tracking_list.txt");
         power_loss_detector = 1110111;
+    }
+    reason = esp_reset_reason();
+    switch (reason)
+    {
+        case ESP_RST_POWERON:
+        case ESP_RST_BROWNOUT:
+            DEBUG_PRINTF("\nReset reason: Power-on or Brown-out reset\n", "");
+            bot.sendMessage(CHAT_ID, "Hello! I am the Soft Location Tracker. I am ON and ready for work!", "");
+            break;
+        case ESP_RST_SW:
+            DEBUG_PRINTF("\nReset reason: Software reset\n", "");
+            bot.sendMessage(CHAT_ID, ("I've successfully rebooted. Current software version " + String(SOFTWARE_VERSION)), "");
+            delay(1000);
+            bot.sendMessage(CHAT_ID, "Is there anything else I can do for you?", "");
+            ft_check_incomming_messages(0);
+            ft_go_to_sleep();
+            break;
+        case ESP_RST_PANIC:
+            DEBUG_PRINTF("\nReset reason: Panic/exception reset\n", "");
+            bot.sendMessage(CHAT_ID, "Don't panic, but my Core has just panicked!", "");
+            delay(3000);
+            bot.sendMessage(CHAT_ID, "You were trying to update me, weren't you? Well, let's try again", "");
+            delay(2000);
+            ft_check_incomming_messages(ft_ota_mode(CHAT_ID));
+            ft_go_to_sleep();
+            break;
     }
 }
